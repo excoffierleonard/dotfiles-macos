4dd125e4925fda73adba66d683063ab53b23691b
Leonard Excoffier <excoffier.leonard@gmail.com>
2024-05-14 16:37:19 -0400

    quick fixes

--- .config/nvim/lua/el/plugins/colorscheme.lua
+++ .config/nvim/lua/el/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        require("gruvbox").setup({
-            -- you can place your customization parameters here
-        })
-        vim.cmd("colorscheme gruvbox")
-    end
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		local transparent = false -- set to true if you would like to enable transparency
+		require("gruvbox").setup({
+			-- you can place your customization parameters here
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
 }

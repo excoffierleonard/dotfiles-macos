1d21d2e00abc4411d113a17d41f2fae7e037cf5a
Leonard Excoffier <excoffier.leonard@gmail.com>
2024-05-14 13:55:52 -0400

    set up lsp in nvim.

--- .config/nvim/lua/el/lazy.lua
+++ .config/nvim/lua/el/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("el.plugins", {
+require("lazy").setup({ { import = "el.plugins" }, { import = "el.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,

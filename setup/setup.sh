4bb571852c46e61270655624c351b188f88e1ba6
Leonard Excoffier <excoffier.leonard@gmail.com>
2024-05-15 12:26:08 -0400

    correctly ordered setup scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,8 +2,15 @@
 
 # Set the setup directory to where this script is located
 SETUP_DIR="$(cd "$(dirname "$0")" && pwd)"
-SYMLINK_SCRIPT="$SETUP_DIR/symlink_dotfiles.sh"
 BREW_SCRIPT="$SETUP_DIR/install_brew_packages.sh"
+SYMLINK_SCRIPT="$SETUP_DIR/symlink_dotfiles.sh"
+
+# Run Homebrew install script
+if [ -f "$BREW_SCRIPT" ]; then
+    bash "$BREW_SCRIPT"
+else
+    echo "Homebrew install script not found: $BREW_SCRIPT"
+fi
 
 # Run symlink script
 if [ -f "$SYMLINK_SCRIPT" ]; then
@@ -12,9 +19,3 @@ else
     echo "Symlink script not found: $SYMLINK_SCRIPT"
 fi
 
-# Run Homebrew install script
-if [ -f "$BREW_SCRIPT" ]; then
-    bash "$BREW_SCRIPT"
-else
-    echo "Homebrew install script not found: $BREW_SCRIPT"
-fi

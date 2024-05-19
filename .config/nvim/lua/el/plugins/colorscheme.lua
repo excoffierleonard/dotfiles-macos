d13dc5e9016faa134800d26c3fdbdd90543061ac
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-05-18 22:15:42 -0400

    changed theme to tokyonight

--- .config/nvim/lua/el/plugins/colorscheme.lua
+++ .config/nvim/lua/el/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
 		local transparent = false -- set to true if you would like to enable transparency
-		require("gruvbox").setup({
+		require("tokyonight").setup({
 			-- you can place your customization parameters here
 		})
-		vim.cmd("colorscheme gruvbox")
+		vim.cmd("colorscheme tokyonight")
 	end,
 }

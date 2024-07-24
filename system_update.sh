9bb1376a28c20c98fdc23a892a874770b9988572
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-07-24 15:05:44 +0200

    Changed update script so Lazy update works.

--- system_update.sh
+++ system_update.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Update Homebrew and dump the new Brewfile
-brew update && brew upgrade && brew bundle dump --describe --force --file="$HOME/.dotfiles/setup/Brewfile"
+brew update 
+brew upgrade 
+brew bundle dump --describe --force --file="$HOME/.dotfiles/setup/Brewfile"
 
 # Update Lazy.nvim plugins
-nvim --headless +Lazy! +qa
+nvim --headless "+Lazy! sync" +qa
 
+# FIXME: Mason Update sometimes does not work.
 # Update Mason plugins
 nvim --headless +"MasonUpdate" +qa
 
 # Commit and push changes to the dotfiles repository
-cd "$HOME/.dotfiles" || { echo "Failed to change directory to ~/.dotfiles"; exit 1; }
+cd "~/.dotfiles" || { echo "Failed to change directory to ~/.dotfiles"; exit 1; }
 git add .
 git commit -m "System Update."
 git push
